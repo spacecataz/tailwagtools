ab3ae6ca5ccbaeaeb11951473429c57322b2e739
yugidoge <yu-gi-doge@protonmail.com>
2020-08-14 11:10:56 -0500

    made it more organized and ive added a few functions...

--- tailwag.py
+++ tailwag.py
@@ -489,23 +489,26 @@ def kp_finder_range(t1, t2):
 def Vz_finder_range(date_str, date_obj, int_hours):
     '''
     This function takes in two datetime objects
-    and finds the range of kp indices as an array
+    and finds the average value of an array of Vz 
+    values over a time interval of specified hours 
+    an array
     
     Parameters
     ==========
-    t1 : datetime
-        The initial date as a datetime object
+    date_str : string
+        your initial date point as a string object
         
-    t2 : datetime
-        The final date as a datetime object
+    date_obj : datetime
+        your initial date point as a datetime object
+        
+    int_hours : int
+        your interval hours as an int
 
     Returns
     =======
-    Kprange : array
-       the values of the kp in an array...
-       
-    UTCrnge : array
-       the values of the time  in an array...
+    avg : float
+       the average value of the VZ array
+    
     '''
 
     import datetime as dt
@@ -644,126 +647,26 @@ def read_ascii(filename):
             
     return data
 
-def omni_to_swmf(infile, outfile=None):
+def to_pickle(data_dic, picklename):
     '''
-    Given an input omni file, *infile*, read its contents and convert to an
-    SWMF input file.  The omni input file **must** have the following
-    variables: BX, BY, BZ (all GSM); Proton Density; Speed; Temperature.
+    Parameters
+    ----------
+    data_dic : dictionary
+        DESCRIPTION.
+    picklename : TYPE
+        DESCRIPTION.
 
-    If the kwarg *outfile* is set, the resulting conversion will be written
-    to the file specified by the kwarg. 
-    
-    Both the omni data and the ImfInput object are returned to the caller.
+    Returns
+    -------
+    None....It just makes a pickle bro, -high five-......
 
-    This function will attempt to linearly interpolate over bad data values.
-    If the first or last line of the data contains bad data flags, this will
-    fail.
-
-    GSE coordinates V_Y and V_Z will be rotated to GSM coordinates.
-
-    Finally, always inspect your results.
     '''
-    import re
-    import datetime as dt
-    import numpy as np
-    from spacepy.datamodel import dmarray
-    from spacepy.pybats import ImfInput
-    from spacepy.time import Ticktock
-    from spacepy.coordinates import Coords
-    from validate import pairtimeseries_linear as pair
-    
-    # Variable name mapping:
-    omVar = ['BX', 'BY', 'BZ', 'Vx Velocity', 'Vy Velocity', 'Vz Velocity', 'Proton Density', 'Temperature']
-    swVar = ['bx', 'by', 'bz', 'ux',          'uy',          'uz',          'rho',            'temp'       ]
-    #omVar = ['BX', 'BY', 'BZ', 'Speed', 'Proton Density', 'Temperature']
+   
+    import pickle
 
-    # Load observations:
-    omdat = read_ascii(infile)
-    nPts = omdat['time'].size
-    
-    # Create empty ImfInput object:
-    swmf = ImfInput(npoints=nPts, load=False, filename=outfile)
-    
-    # Convert data and load into ImfInput object:
-    swmf['time'] = omdat['time']
-    npts = swmf['time'].size
-    for vo, vs in zip(omVar, swVar):
-        if vo in omdat:
-            swmf[vs] = pair(omdat['time'], omdat[vo], omdat['time'])
-        else:
-            print('WARNING: Did not find {} in Omni file.'.format(vo))
-            swmf[vs] = np.zeros(npts)
-
-    # Do rotation of Vy, Vz GSE->GSM:
-    rot = Coords(np.array([swmf['ux'], swmf['uy'], swmf['uz']]).transpose(),
-                 'GSE', 'car', ['Re','Re','Re'],
-                 Ticktock(swmf['time'])).convert('GSM', 'car')
-
-    # Copy rotated values into original structure:
-    swmf['ux'] = rot.data[:,0]
-    swmf['uy'] = rot.data[:,1]
-    swmf['uz'] = rot.data[:,2]
-            
-    # Flip sign of velocity:
-    if swmf['ux'].min() >0: swmf['ux'] *= -1
-
-    if outfile: swmf.write()
-        
-    return omdat, swmf
-
-def qindent_to_swmf(infiles, outfile):
-    '''
-    Given either a single input file name or a list of multiple file names
-    corresponding to JSON-headed ASCII Qin-Denton OMNI data (obtainable from
-    http://www.rbsp-ect.lanl.gov/data_pub/QinDenton), this function will
-    produce an SWMF IMF input file.
-
-    If the kwarg *outfile* is set, the resulting conversion will be written
-    to the file specified by the kwarg. 
-    
-    Both the omni data and the ImfInput object are returned to the caller.
-
-    This function will attempt to linearly interpolate over bad data values.
-    If the first or last line of the data contains bad data flags, this will
-    fail.
-
-    There are many limitations to this approach.  First, V_Y and V_Z are
-    set to zero as this information is not given in the Qin-Denton files.
-    The same is true for IMF Bx.  Finally, and most importantly, solar
-    wind temperature is not present, making this whole thing really bad.
-
-    A limitation of this approach is that solar V_Y, V_Z are neglected.  
-    Additionally, there is no B_X information, so these values are set to
-    zero as well.  Use with care!
-
-    Finally, always inspect your results.
-    '''
-
-    import re
-    import datetime as dt
-    import numpy as np
-    from spacepy.datamodel import dmarray
-    from spacepy.omni import readJSONheadedASCII as readit
-    from spacepy.pybats import ImfInput
-    from validate import pairtimeseries_linear as pair
-
-    raise NotImplemented('This function is incomplete.')
-    
-    return False
-    
-    # Variable names:
-    omVar = ['ByIMF', 'BzIMF', 'Vsw', 'Den_P', 'Temperature']
-    swVar = ['by',    'bz',    'ux',  'rho',   'temp'       ]
-
-    # Read omni files:
-    omdat = readit(infiles)
-
-    # Create empty ImfInput object:
-    nPts = omdat['time'].size
-    swmf = ImfInput(npoints=nPts, load=False, filename=outfile)
-    
-    return omdat, swmf
-
+    pickle_out = open(picklename, "wb")
+    pickle.dump(data_dic, pickle_out)
+    pickle_out.close()
 
 
 
