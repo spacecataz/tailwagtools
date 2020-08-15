37b9cbfbad01034a217a09c04e446f3479bea1b7
yugidoge <yu-gi-doge@protonmail.com>
2020-08-15 11:08:38 -0500

    heres fuze, set up for the wind graph atm...

--- FUZE.py
+++ FUZE.py
@@ -47,19 +47,12 @@ def main():
     print("THIS IS THE BEGINNING OF THE PROGRAM:  \n")   
     print(spacepy.plot.available(returnvals = False))
     
+    print("Hello there, I wanna play a game")
     print()
-    
-    
+
     ####  swiping the event points from the excel file that were gonna use
     Event_Data = pd.read_excel("Event_Points.xlsx", header = 0)
     
-
-    
-    
-    
-   
-
-
     ####  getting three arrays: Point_list as strings, Date_List as datetimes, and the used cluster satellite
     Point_List = Event_Data['Narrowed Point']
     Date_List  = [dt.datetime.strptime(date,'%Y-%m-%d %H:%M:%S.%f') for date in Point_List]
@@ -67,114 +60,34 @@ def main():
     Op_List    = Event_Data['Operation']
    
    
-    ##for (a, b, c) in zip(Date_List, Sat_List, Op_List):
-       ## if c == "SKIP":
-        ##    continue
-      ##  else:======
-          ##  fig, ax1, ax2, ax3, ax4, ax5, n = tw.fusion(a, b, 12, outdir = 'fusion_plots/')
     
     
     
     
-    ## SET UP THE ARRAY THAT WERE GOING TO NEED FOR PLOTTING
-    Vz_89Values       = []
-    Vz_96Values       = []
-    Vz_01Values       = []
-    T89_diff_Values   = []
-    T96_diff_Values   = []
-    T01_diff_Values   = []
+
     
+    
+def main():
+    print("THIS IS THE BEGINNING OF THE PROGRAM:  \n")   
+    print(spacepy.plot.available(returnvals = False))
+    
+    print("Hello there, I wanna play a game")
+    print()
+
+    ####  swiping the event points from the excel file that were gonna use
+    Event_Data = pd.read_excel("Event_Points.xlsx", header = 0)
+    
+    ####  getting three arrays: Point_list as strings, Date_List as datetimes, and the used cluster satellite
+    Point_List = Event_Data['Narrowed Point']
+    Date_List  = [dt.datetime.strptime(date,'%Y-%m-%d %H:%M:%S.%f') for date in Point_List]
+    Sat_List   = Event_Data['USED SAT']
+    Op_List    = Event_Data['Operation']
+    
+    tw.wind_graph(Date_List, Op_List)
     
         
     
-    for (a, b, c) in zip(Date_List, Sat_List, Op_List):
-         if c == "SKIP":
-            continue
-         else:
-             print(type(a))
-             print(a)   
-             
-             
-             ##### STEP 1 GET ALL THE DATA THAT WERE GONNA NEED ############
-             ### FETCH TSYG DATA HERE!  
-              ######## STEP 2 START APPENDING THE VALUES TO THE ARRAYS #############
-             my_clus, my_ttimes, bork_after, bork_before = scatter.get_crossing_info(a, debug=False)
-             Vz_val = tw.Vz_finder_range(str(my_clus[0]), my_clus[0], 2)
-             print("bork")
-             
-             
-             print("BRO THIS IS THE cluster date:  " + str(my_clus[0]))
-             print("YO BRO THIS IS THE DATE FROM T89:  " + str(my_ttimes['T89']))
-             print("YO BRO THIS IS THE DATE FROM T96:  " + str(my_ttimes['T96']))
-             print("YO BRO THIS IS THE DATE FROM T01storm:  " + str(my_ttimes['T01STORM']))
-             
-             if isinstance(my_ttimes['T89'], dt.datetime):
-                 print("AWWWEEE SHEEEET DAWG 89 is A DATETIME")
-                 timediff89 = my_clus[0] - my_ttimes['T89']
-                 minutediff89 = int(timediff89.total_seconds() / 60)                
-                 print("The time difference (in minutes) for T89 is:   " + str(minutediff89))
-                 T89_diff_Values.append(minutediff89)
-                 Vz_89Values.append(Vz_val)
-                 
-             else:
-                 print("THIS IS ACTUALLY A NAN")
-                 
-             if isinstance(my_ttimes['T96'], dt.datetime):
-                 print("AWWWEEE SHEEEET DAWG 89 is A DATETIME")
-                 timediff96 = my_clus[0] - my_ttimes['T96']
-                 minutediff96 = int(timediff96.total_seconds() / 60)
-                 print("The time difference (in minutes) for T89 is:   " + str(minutediff96))
-                 T96_diff_Values.append(minutediff96)
-                 Vz_96Values.append(Vz_val)
-                 
-             else:
-                 print("THIS IS ACTUALLY A NAN")
-    
-             if isinstance(my_ttimes['T01STORM'], dt.datetime):
-                 print("AWWWEEE SHEEEET DAWG 89 is A DATETIME")
-                 timediff01 = my_clus[0] - my_ttimes['T01STORM']
-                 minutediff01 = int(timediff01.total_seconds() / 60)          
-                 print("The time difference (in minutes) for T89 is:   " + str(minutediff01))
-                 T01_diff_Values.append(minutediff01)
-                 Vz_01Values.append(Vz_val)
-                 
-                 
-             else:
-                 print("THIS IS ACTUALLY A NAN")
-    
-    
-    
-    for (a, b) in zip(Vz_89Values, T89_diff_Values):
-        print(str(a) + "          " + str(b))
-        
-    ######## ###########STEP 3 LETS MAKE THAT GRAPH!!!!!! ###############################
-             
-   
-    plt.scatter(Vz_89Values, T89_diff_Values, color='green',  label='T89', alpha=0.5)    
-    plt.scatter(Vz_96Values, T96_diff_Values, color='orange', label='T96', alpha=0.5)
-    plt.scatter(Vz_01Values, T01_diff_Values, color='crimson',label='T01', alpha=0.5)
-    
-    trend89 = np.polyfit(Vz_89Values, T89_diff_Values, 1)
-    trendpoly89 = np.poly1d(trend89) 
-    plt.plot(Vz_89Values,trendpoly89(Vz_89Values), color = 'green', lw=.5)
-    
-    trend96 = np.polyfit(Vz_96Values, T96_diff_Values, 1)
-    trendpoly96 = np.poly1d(trend96) 
-    plt.plot(Vz_96Values,trendpoly96(Vz_96Values), color = 'orange', lw=.5)
-    
-    trend01 = np.polyfit(Vz_01Values, T01_diff_Values, 1)
-    trendpoly01 = np.poly1d(trend01) 
-    plt.plot(Vz_01Values,trendpoly01(Vz_01Values), color = 'crimson', lw=.5)
-    
-    
-    
-    
-    plt.title('Vz and time Differential graph')
-    plt.xlabel('Vz Values')
-    plt.ylabel('Time differentials')
-    plt.legend(loc='best')
-    plt.show()
-             
+ 
              
            
 
