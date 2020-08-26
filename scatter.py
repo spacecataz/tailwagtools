326a79adcadaa0d99c35c6b8f771536c17f2c2f2
yugidoge <yu-gi-doge@protonmail.com>
2020-08-26 16:38:01 -0500

    changed scatter back so that it runs correctly please dont hurt me Michelle : (

--- scatter.py
+++ scatter.py
@@ -71,7 +71,7 @@ def read_Event_Points(filename):
     return epoch
 
 
-def get_crossing_info(epoch, check_graph = False, debug=False):
+def get_crossing_info(epoch, debug=False):
     '''
     For a given crossing epoch, calculate and return:
     - the crossing time for all Tsgyanenko models under consideration
@@ -117,7 +117,7 @@ def get_crossing_info(epoch, check_graph = False, debug=False):
         'T01STORM': dmarray([datetime.datetime(...)], dtype=object)
         }
     '''
-
+    
     #Fetch cluster data given an epoch
     data = tailwag.fetch_cluster_data(epoch) #get Cluster data
     loc = np.abs(data['b'][:,0])==np.abs(data['b'][:,0]).min() #location of Cluster crossing
@@ -127,7 +127,7 @@ def get_crossing_info(epoch, check_graph = False, debug=False):
     t_cis = data['cis_time'] #get Cluster CIS times
     h_dens = data['dens_h']
     o_dens = data['dens_o']
-
+    
     #Location t_cis closest to t_cluster
     #MXB notes: should we change this into tricky indexing instead of np.where? this is probably too chonky
     index_cluster = np.where(t_cis<=t_cluster)[0][-1] #locate CIS time <= cluster crossing time
@@ -142,47 +142,142 @@ def get_crossing_info(epoch, check_graph = False, debug=False):
         print(f'\tCrossing epoch from file: {epoch}')
         print(f'\tCrossing time from CIS epochs: {t_cis[index_cluster]}')
         
-    if check_graph == True:
-        #Calculate Cluster average density after
-        loc = t_cis>t_cis[index_cluster] #location after Cluster crossing
-        cluster_o_after = o_dens[loc].mean() 
-        cluster_h_after = h_dens[loc].mean() 
-    
-        #Calculate Cluster average density before
-        loc = t_cis<t_cis[index_cluster] #location before Cluster crossing
-        cluster_o_before = o_dens[loc].mean() #average H+ density + average O+ density
-        cluster_h_before = h_dens[loc].mean() #average H+ density + average O+ density
-        
-        return(cluster_h_before, cluster_o_before, cluster_h_after, cluster_o_after)
-    
-    else:
-        
+     
     
         #Calculate Cluster average density after
-        loc = t_cis>t_cis[index_cluster] #location after Cluster crossing
-        cluster_after = h_dens[loc].mean() + o_dens[loc].mean() #average H+ density + average O+ density
+    loc = t_cis>t_cis[index_cluster] #location after Cluster crossing
+    cluster_after = h_dens[loc].mean() + o_dens[loc].mean() #average H+ density + average O+ density
     
         #Calculate Cluster average density before
-        loc = t_cis<t_cis[index_cluster] #location before Cluster crossing
-        cluster_before = h_dens[loc].mean() + o_dens[loc].mean() #average H+ density + average O+ density
+    loc = t_cis<t_cis[index_cluster] #location before Cluster crossing
+    cluster_before = h_dens[loc].mean() + o_dens[loc].mean() #average H+ density + average O+ density
     
 
         #Get crossing time and densities for each Tsyg model
-        t_times = {} #container for times
-        index_Tsyg = {}
+    t_times = {} #container for times
+    index_Tsyg = {}
 
-        for vers in ['T89','T96', 'T01STORM']:
+    for vers in ['T89','T96', 'T01STORM']:
             #Obtain Tsyg crossing times
-            t, b_Tsyg = tailwag.gen_sat_tsyg(data, extMag = vers) #get Tsyg data
-            loc = np.abs(b_Tsyg[:,0])==np.abs(b_Tsyg[:,0]).min() #location of crossing
+        t, b_Tsyg = tailwag.gen_sat_tsyg(data, extMag = vers) #get Tsyg data
+        loc = np.abs(b_Tsyg[:,0])==np.abs(b_Tsyg[:,0]).min() #location of crossing
             # It's possible to have no results from a given Tsyg model.
             # If that's the case, return NaNs.
-            if t[loc].size > 0:
-                t_times[vers] = t[loc][0] #Tsyg crosing time
-            else:
+        if t[loc].size > 0:
+            t_times[vers] = t[loc][0] #Tsyg crosing time
+        else:
                 t_times[vers] = np.nan
+    
+    return(t_cluster, t_times, cluster_after, cluster_before)
 
-        return(t_cluster, t_times, cluster_after, cluster_before)
+
+def split_crossing_info(epoch, debug=False):
+    '''
+    For a given crossing epoch, calculate and return:
+    - the crossing time for all Tsgyanenko models under consideration
+    - the average density before and after the cluster crossing
+      for both H+ and O+
+
+    Parameters
+    ----------
+    epoch : datetime.datetime
+       The time of the cluster crossing of the plasmasheet.
+
+    Other Parameters
+    ----------------
+    debug : boolean
+       Turn on debug information.  Defaults to False
+
+    Returns
+    -------
+    t_cluster : datetime.datetime
+        Time of crossing observed in Cluster
+    t_times : array of datetime.datetime
+        Time of crossing observed in T89, T96, AND T01STORM
+        T89 =  t_times[0], T96 = t_times[1], AND T01STORM = t_times[2]      
+    cluster_before : array of float values
+        An array of values that yield the average value before 
+        the time crossing.. ...
+        cluster_before[0] = H value, cluster_before[1] = O value
+    cluster_after : array of float values
+        An array of values that yield the average value after 
+        the time crossing.. ...
+        cluster_after[0] = H value, cluster_after[1] = O value
+
+
+    Example
+    _______
+    >>> import datetime as dt
+    >>> import scatter
+    >>> epoch = dt.datetime(2001,8,19,20,0,0)
+    >>> t_times = scatter.get_crossing_info(epoch)
+    >>> print(t_times)
+    {'T89': dmarray([datetime.datetime(...)], dtype=object),
+        'T96': dmarray([datetime.datetime(...)], dtype=object),
+        'T01STORM': dmarray([datetime.datetime(...)], dtype=object)
+        }
+    '''
+    
+    #Fetch cluster data given an epoch
+    data = tailwag.fetch_cluster_data(epoch) #get Cluster data
+    loc = np.abs(data['b'][:,0])==np.abs(data['b'][:,0]).min() #location of Cluster crossing
+    t_cluster = data['fgm_time'][loc] #Cluster crossing time
+    
+    #Fetch cluster plasma data
+    t_cis = data['cis_time'] #get Cluster CIS times
+    h_dens = data['dens_h']
+    o_dens = data['dens_o']
+    
+    #Location t_cis closest to t_cluster
+    #MXB notes: should we change this into tricky indexing instead of np.where? this is probably too chonky
+    index_cluster = np.where(t_cis<=t_cluster)[0][-1] #locate CIS time <= cluster crossing time
+    tdelt_before = t_cluster-t_cis[index_cluster] #timedelta using t_cis less than t_cluster
+    
+    
+    ##print("The tdeltbefore is the following:     " + str(tdelt_before))
+    ##print("The tcis index cluster + 1 is the following:     " + str(t_cis[index_cluster+1]))
+    ##print("The tcluster is the following:     " + str(t_cluster))
+   
+    tdelt_after = t_cis[index_cluster+1]-t_cluster #timedelta using t_cis more than t_cluster
+    if tdelt_before > tdelt_after: #compare the two, if the timedelta of t_cis less > t_cis more, then change index_cluster to the location of t_cis more
+        index_cluster = index_cluster+1
+
+    # Print some debug information:
+    if debug:
+        print('----------DEBUG----------')
+        print(f'\tCrossing epoch from file: {epoch}')
+        print(f'\tCrossing time from CIS epochs: {t_cis[index_cluster]}')
+        
+     
+    cluster_before = []
+    cluster_after  = []
+        #Calculate Cluster average density after
+    loc = t_cis>t_cis[index_cluster] #location after Cluster crossing
+    cluster_after.append(h_dens[loc].mean()) #average H+ density + average O+ density
+    cluster_after.append(o_dens[loc].mean())
+        #Calculate Cluster average density before
+    loc = t_cis<t_cis[index_cluster] #location before Cluster crossing
+    cluster_before.append(h_dens[loc].mean()) #average H+ density + average O+ density
+    cluster_before.append(o_dens[loc].mean())
+    
+
+        #Get crossing time and densities for each Tsyg model
+    t_times = {} #container for times
+    index_Tsyg = {}
+
+    for vers in ['T89','T96', 'T01STORM']:
+            #Obtain Tsyg crossing times
+        t, b_Tsyg = tailwag.gen_sat_tsyg(data, extMag = vers) #get Tsyg data
+        loc = np.abs(b_Tsyg[:,0])==np.abs(b_Tsyg[:,0]).min() #location of crossing
+            # It's possible to have no results from a given Tsyg model.
+            # If that's the case, return NaNs.
+        if t[loc].size > 0:
+            t_times[vers] = t[loc][0] #Tsyg crosing time
+        else:
+                t_times[vers] = np.nan
+    
+
+    return(t_cluster, t_times, cluster_after, cluster_before)
 
 
 if __name__ == '__main__':
